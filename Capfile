fd3b9a08860558d54ee7c0759b57b6306231430f
Paul Daigle <paul@bitpay.com>
2015-06-22 12:28:10 -0500

    still monkeying around with unicorn

--- Capfile
+++ Capfile
@@ -6,7 +6,7 @@ require 'capistrano/deploy'
 require 'capistrano/rails'
 require 'capistrano/bundler'
 require 'capistrano/rvm'
-require 'capistrano3-unicorn'
+require 'capistrano3/unicorn'
 #
 # Include tasks from other gems included in your Gemfile
 #
--- config/deploy.rb
+++ config/deploy.rb
@@ -39,15 +39,20 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      unicorn:reload
-      unicorn:restart
-      unicorn:duplicate
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
     end
   end
+
+  after 'deploy:publishing', 'deploy:restart'
+  namespace :deploy do
+    task :restart do
+      invoke 'unicorn:restart'
+    end
+  end
+
 
 end
 
